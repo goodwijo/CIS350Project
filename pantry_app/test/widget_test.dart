0d10db26a73311e3a01d505a2c5320c2ad6c5853
Cody Peyerk <peyerkco@mail.gvsu.edu>
2021-11-17 13:23:56 -0500

    Modified base app to be blank

--- pantry_app/test/widget_test.dart
+++ pantry_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pantry_app/main.dart';
+import 'package:pantry_app/app/services/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
