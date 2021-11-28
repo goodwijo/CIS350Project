989bedacede32adc69249fb0210525e6a4cdb7d8
Nick <cartenic@mail.gvsu.edu>
2021-11-27 22:02:44 -0500

    Added Value Notifier

--- pantry_app/lib/app/sign_in/sign_in_bloc.dart
+++ pantry_app/lib/app/sign_in/sign_in_bloc.dart
@@ -4,24 +4,16 @@ import 'package:pantry_app/services/auth.dart';
 import 'package:flutter/foundation.dart';
 
 class SignInBloc {
-  SignInBloc({required this.auth});
+  SignInBloc({required this.auth, required this.isLoading});
   final AuthBase auth;
-
-  final StreamController<bool> _isLoadingController = StreamController<bool>();
-  Stream<bool> get isLoadingStream => _isLoadingController.stream;
-
-  void dispose() {
-    _isLoadingController.close();
-  }
-
-  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+  final ValueNotifier<bool> isLoading;
 
   Future<User?> _signIn(Future<User?> Function() signInMethod) async {
     try {
-      _setIsLoading(true);
+      isLoading.value = true;
       return await signInMethod();
     } catch (e) {
-      _setIsLoading(false);
+      isLoading.value = false;
       rethrow;
     }
   }
